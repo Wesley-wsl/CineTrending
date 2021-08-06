cd44ff692bc8558a3030f2934a8f89d63f58f1b3
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-06 19:17:22 -0300

    :wrench: Added eslint, prettier and editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
