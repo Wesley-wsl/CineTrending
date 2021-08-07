aa775ec1d940b953c6f11a2680f1491011b5d3c2
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-07 15:44:23 -0300

    :art: style: Added global style

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+    font-family: Roboto, Arial, Helvetica, sans-serif;
+    box-sizing:border-box;
+}
+
+`;
+
+export default Global;
