c4240f80a644f1c83b6837eaa6ff0ad63a62846c
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-08 15:13:50 -0300

    :wrench: feat: Created Footer and your style.

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { FooterStyle } from './style';
+
+export default function Footer() {
+    return (
+        <FooterStyle>
+            <p>
+                MovieApp - 2021 <br />
+                <span>
+                    powered by <a href="https://www.themoviedb.org">tmdb</a>
+                </span>
+            </p>
+        </FooterStyle>
+    );
+}
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const FooterStyle = styled.footer`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    background-color: #00020b;
+    padding: 30px;
+    margin-top: 150px;
+    height: 200px;
+    p {
+        font-size: 35px;
+        margin-left: 3%;
+
+        span {
+            font-size: 15px;
+            opacity: 0.7;
+        }
+    }
+`;
