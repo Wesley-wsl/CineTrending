8690764a773b7aea3852fbfefca10f88e4ffec94
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-08 15:14:46 -0300

    :wrench: feat: Added Footer in App

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home/index';
 import Header from '.././components/Header/index';
+import Footer from '../components/Footer/index';
 
 export default function Router() {
     return (
@@ -9,6 +10,7 @@ export default function Router() {
             <Switch>
                 <Route path="/" exact component={Home} />
             </Switch>
+            <Footer />
         </>
     );
 }
