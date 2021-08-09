54095ecc38fefe9edcc19f4aad10a029b796fb50
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-09 18:43:21 -0300

    :art: style: new style for Hero Image

---
+++ src/pages/Home/style.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const HeroImage = styled.section`
+    background-color: #000315;
+    background-image: url('https://image.tmdb.org/t/p/original/jlGmlFOcfo8n5tURmhC7YVd4Iyy.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 500px;
+    color: #000315;
+`;
+
+export const ListName = styled.div`
+    h2 {
+        font-size: 30px;
+        margin-top: 50px;
+        margin-bottom: 30px;
+        margin-left: 5%;
+    }
+`;
