6c83210f4d09651776d364a0d0fcf1b483b86a52
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-13 14:40:44 -0300

    :wrench: feat: Added react-scroll

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,27 @@
 import { HeaderMain } from './style';
+import { Link } from 'react-scroll';
 
 export default function Header() {
     return (
         <HeaderMain>
-            <h1>CineTrending</h1>
+            <h1 id="Home">CineTrending</h1>
 
             <nav>
                 <ul>
                     <li>
-                        <a href="#">Home</a>
+                        <Link to="Home" smooth={true} duration={1000}>
+                            Home
+                        </Link>
                     </li>
                     <li>
-                        <a href="#Movies">Top Movies</a>
+                        <Link to="Movies" smooth={true} duration={900}>
+                            Top Movies
+                        </Link>
                     </li>
                     <li>
-                        <a href="#TVs">Top TVs</a>
+                        <Link to="TVs" smooth={true} duration={900}>
+                            Top TVs
+                        </Link>
                     </li>
                 </ul>
             </nav>
