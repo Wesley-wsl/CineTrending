9b0689d9dd79dd05db12cedf568cce7d6c2ad4d9
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-13 14:44:35 -0300

    :hammer: feat: Added new router for details movies/tvs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home/index';
 import Header from '.././components/Header/index';
 import Footer from '../components/Footer/index';
+import Details from '../pages/Details/index';
 
 export default function Router() {
     return (
@@ -9,6 +10,7 @@ export default function Router() {
             <Header />
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/details/:id" exact component={Details} />
             </Switch>
             <Footer />
         </>
