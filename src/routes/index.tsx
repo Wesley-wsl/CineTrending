11cb48b6064aa44cd2304f1f5673a2150e7b1ac1
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-14 17:18:49 -0300

    :hammer: Created new routers

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import Home from '../pages/Home/index';
 import Header from '.././components/Header/index';
 import Footer from '../components/Footer/index';
 import Details from '../pages/Details/index';
+import DetailsTV from '../pages/DetailsTV';
+import NotFound from '../pages/NotFound';
 
 export default function Router() {
     return (
@@ -11,6 +13,8 @@ export default function Router() {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/details/:id" exact component={Details} />
+                <Route path="/details/tv/:id" exact component={DetailsTV} />
+                <Route path="*" exact component={NotFound} />
             </Switch>
             <Footer />
         </>
