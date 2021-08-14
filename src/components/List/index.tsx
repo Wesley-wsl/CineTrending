8074506309c2501070dc9475b36c03b6bd403c02
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-14 17:14:30 -0300

    :zap: feat: Added Link for next page

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,26 +1,50 @@
 import { ListStyle } from './styles';
+import { Link } from 'react-router-dom';
 
-export function List(props: any) {
+type MovieProps = {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    movie: Array<Object>;
+    original_title: string;
+    original_name: string;
+    release_date: string;
+    first_air_date: string;
+    id: string;
+    poster_path: string;
+};
+
+type ListProps = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    listRenderWith: any;
+    isMovie: boolean;
+};
+
+export function List(props: ListProps) {
     return (
         <ListStyle>
-            {props.listRenderWith.map((movie: any, index: number) => {
+            {props.listRenderWith.map((movie: MovieProps, index: number) => {
                 if (index < 10) {
                     return (
                         <div key={index}>
-                            <img
-                                src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-                                alt="Poster"
-                            />
-                            <p>
-                                {movie.original_title
-                                    ? movie.original_title
-                                    : movie.original_name}
-                            </p>
-                            <p className="year">
-                                {movie.release_date
-                                    ? movie.release_date
-                                    : movie.first_air_date}
-                            </p>
+                            <Link
+                                to={`./details${props.isMovie ? '' : '/tv'}/${
+                                    movie.id
+                                }`}
+                            >
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+                                    alt="Poster"
+                                />
+                                <p>
+                                    {movie.original_title
+                                        ? movie.original_title
+                                        : movie.original_name}
+                                </p>
+                                <p className="year">
+                                    {movie.release_date
+                                        ? movie.release_date
+                                        : movie.first_air_date}
+                                </p>
+                            </Link>
                         </div>
                     );
                 }
