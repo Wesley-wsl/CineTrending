c2b4e1cb992a86ed059229e5af05f97b6c938be2
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-19 15:23:33 -0300

    :wrench: feat: add listName dynamic

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,31 +1,37 @@
 import { Link } from 'react-router-dom';
 
 import { IList, IMovie } from '../../@types';
+import { ListName } from '../../pages/Home/styles';
 import { ListStyle } from './styles';
 
-export function List({ listRenderWith, isMovie }: IList) {
+export function List({ listRenderWith, isMovie, listName }: IList) {
     return (
-        <ListStyle>
-            {listRenderWith.map((movie: IMovie, index: number) => (
-                <div key={index}>
-                    <Link to={`./details${isMovie ? '' : '/tv'}/${movie.id}`}>
-                        <img
-                            src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-                            alt="Poster"
-                        />
-                        <p>
-                            {movie.original_title
-                                ? movie.original_title
-                                : movie.original_name}
-                        </p>
-                        <p className="year">
-                            {movie.release_date
-                                ? movie.release_date
-                                : movie.first_air_date}
-                        </p>
-                    </Link>
-                </div>
-            ))}
-        </ListStyle>
+        <>
+            <ListName>{listName}</ListName>
+            <ListStyle>
+                {listRenderWith.map((movie: IMovie, index: number) => (
+                    <div key={index}>
+                        <Link
+                            to={`/details${isMovie ? '' : '/tv'}/${movie.id}`}
+                        >
+                            <img
+                                src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+                                alt="Poster"
+                            />
+                            <p>
+                                {movie.original_title
+                                    ? movie.original_title
+                                    : movie.original_name}
+                            </p>
+                            <p className="year">
+                                {movie.release_date
+                                    ? movie.release_date
+                                    : movie.first_air_date}
+                            </p>
+                        </Link>
+                    </div>
+                ))}
+            </ListStyle>
+        </>
     );
 }
