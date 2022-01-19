3d588e57c004df9f1108b6834fc4f02dce2b3bc0
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-19 15:14:59 -0300

    :wrench: feat: create template to pages details

---
+++ src/templates/DetailsTemplate/index.tsx
@@ -0,0 +1,57 @@
+import { IDetailsTemplate } from '../../@types';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import * as S from './styles';
+
+const DetailsTemplate: React.FC<IDetailsTemplate> = ({ data, trailer }) => {
+    return (
+        <>
+            <Header />
+
+            <S.DetailsStyle>
+                <S.BackgroundImage
+                    className="background"
+                    src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
+                    alt="background"
+                />
+
+                <S.MainInformations>
+                    <S.Poster
+                        className="poster"
+                        src={`https://image.tmdb.org/t/p/w185${data.poster_path}`}
+                        alt="Poster"
+                    />
+                    <div>
+                        <h2>
+                            {data.original_title
+                                ? data.original_title
+                                : data.original_name}
+                        </h2>
+
+                        <span>
+                            <i className="fas fa-star"></i> {data.vote_average}
+                        </span>
+
+                        <button>
+                            <a
+                                href={`https://www.youtube.com/watch?v=${trailer}`}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Watch Trailer{' '}
+                            </a>
+                        </button>
+                    </div>
+                </S.MainInformations>
+
+                <S.Overview>
+                    <h2>Overview</h2>
+                    <p>{data.overview}</p>
+                </S.Overview>
+            </S.DetailsStyle>
+            <Footer />
+        </>
+    );
+};
+
+export default DetailsTemplate;
