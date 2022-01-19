7fc005431e1655be14169e6111c0fd242b382cbc
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-19 15:21:15 -0300

    :recycle: refactor: use DetailsTemplate

--- src/pages/DetailsTV/index.tsx
+++ src/pages/DetailsTV/index.tsx
@@ -1,8 +1,9 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { BackgroundImage, DetailsStyle } from '../Details/styles';
+import Loading from '../../components/Loading';
+import { api, keyApi } from '../../services/api';
+import DetailsTemplate from '../../templates/DetailsTemplate';
 
 type Params = {
     id?: string | number;
@@ -13,102 +14,31 @@ const DetailsTV: React.FC = () => {
     const { id }: Params = useParams();
     const [data, setData] = useState(Object);
     const [trailer, setTrailer] = useState(Object);
-    window.scrollTo(0, 0);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
-    function getInfo() {
-        axios
-            .get(
-                `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.REACT_APP_IMDB_API_KEY}&language=en-US`,
-            )
-            .then(res => {
-                setData(res.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    async function getInfo() {
+        await api
+            .get(`tv/${id}?api_key=${keyApi}&language=en-US`)
+            .then(res => setData(res.data));
     }
 
-    function getTrailer() {
-        axios
-            .get(
-                `https://api.themoviedb.org/3/tv/${id}/videos?api_key=${process.env.REACT_APP_IMDB_API_KEY}&language=en-US`,
-            )
-            .then(res => {
-                res.data.results[0] ? setTrailer(res.data.results[0].key) : '';
-            });
+    async function getTrailer() {
+        await api
+            .get(`tv/${id}/videos?api_key=${keyApi}&language=en-US`)
+            .then(res => setTrailer(res.data.results[0].key));
     }
 
     useEffect(() => {
-        getInfo();
-        getTrailer();
+        Promise.all([getInfo(), getTrailer()])
+            .then(() => setTimeout(() => setLoading(false), 400))
+            .catch(() => navigate('/'));
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [id]);
+    }, []);
 
-    return (
-        <DetailsStyle>
-            <div>
-                <img
-                    className="poster"
-                    src={`https://image.tmdb.org/t/p/w185${data.poster_path}`}
-                    alt="Poster"
-                />
-                {data ? (
-                    <BackgroundImage>
-                        <img
-                            className="background"
-                            src={`https://image.tmdb.org/t/p/w780${data.backdrop_path}`}
-                            alt="background"
-                        />
-                    </BackgroundImage>
-                ) : (
-                    'Loading...'
-                )}
+    if (loading) return <Loading />;
 
-                <div>
-                    <h2>{data.original_name}</h2>
-                    <p>
-                        <span>OverView</span>: {data.overview}
-                    </p>
-                    <p>
-                        <span>Companies</span>:{' '}
-                        {data.production_companies
-                            ? data.production_companies.map(
-                                  (companies: { name: string }) =>
-                                      `${companies.name} | `,
-                              )
-                            : 'Loading...'}
-                    </p>
-                    <p>
-                        <span>Genres</span>:{' '}
-                        {data.genres
-                            ? data.genres.map(
-                                  (genre: { name: string }) =>
-                                      `${genre.name} | `,
-                              )
-                            : 'Loading...'}
-                    </p>
-                    <p>
-                        <span>Season Number</span>: {data.number_of_seasons}
-                        {' - '}
-                        <span>Duration</span>:{' '}
-                        {data.episode_run_time
-                            ? `${data.episode_run_time[0]} min`
-                            : 'Undefined'}
-                    </p>
-                    <span>
-                        <i className="fas fa-star"></i> {data.vote_average}
-                    </span>
-                    <a
-                        href={`https://www.youtube.com/watch?v=${trailer}`}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <button> Watch Trailer</button>
-                    </a>
-                </div>
-            </div>
-        </DetailsStyle>
-    );
+    return <DetailsTemplate data={data} trailer={trailer} />;
 };
 
 export default DetailsTV;
