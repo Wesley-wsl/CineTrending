2ff0ec75bb058fd0496f97512720323103f80148
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-19 15:22:21 -0300

    :pencil2: chore: update types to Details page

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,4 +1,8 @@
 export interface IMovie {
+    title: string;
+    backdrop_path: string;
+    vote_average: number;
+    overview: string;
     original_title: string;
     original_name: string;
     release_date: string;
@@ -10,4 +14,16 @@ export interface IMovie {
 export interface IList {
     listRenderWith: IMovie[];
     isMovie: boolean;
+    listName: string;
+}
+
+export interface IDetailsTemplate {
+    data: IMovie;
+    trailer: string;
+}
+
+export interface IParams {
+    id?: string | number;
+    idTv?: string | number;
+    movie?: string;
 }
