47bee940e7b71e21cc6c402bdb88fab1c58af88e
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-19 10:19:38 -0300

    :recycle: refactor: clear code and move types to @types

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,55 +1,31 @@
 import { Link } from 'react-router-dom';
 
+import { IList, IMovie } from '../../@types';
 import { ListStyle } from './styles';
 
-type MovieProps = {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    movie: Array<Object>;
-    original_title: string;
-    original_name: string;
-    release_date: string;
-    first_air_date: string;
-    id: string;
-    poster_path: string;
-};
-
-type ListProps = {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    listRenderWith: any;
-    isMovie: boolean;
-};
-
-export function List(props: ListProps) {
+export function List({ listRenderWith, isMovie }: IList) {
     return (
         <ListStyle>
-            {props.listRenderWith.map((movie: MovieProps, index: number) => {
-                if (index < 10) {
-                    return (
-                        <div key={index}>
-                            <Link
-                                to={`./details${props.isMovie ? '' : '/tv'}/${
-                                    movie.id
-                                }`}
-                            >
-                                <img
-                                    src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-                                    alt="Poster"
-                                />
-                                <p>
-                                    {movie.original_title
-                                        ? movie.original_title
-                                        : movie.original_name}
-                                </p>
-                                <p className="year">
-                                    {movie.release_date
-                                        ? movie.release_date
-                                        : movie.first_air_date}
-                                </p>
-                            </Link>
-                        </div>
-                    );
-                }
-            })}
+            {listRenderWith.map((movie: IMovie, index: number) => (
+                <div key={index}>
+                    <Link to={`./details${isMovie ? '' : '/tv'}/${movie.id}`}>
+                        <img
+                            src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+                            alt="Poster"
+                        />
+                        <p>
+                            {movie.original_title
+                                ? movie.original_title
+                                : movie.original_name}
+                        </p>
+                        <p className="year">
+                            {movie.release_date
+                                ? movie.release_date
+                                : movie.first_air_date}
+                        </p>
+                    </Link>
+                </div>
+            ))}
         </ListStyle>
     );
 }
