2ffcc78955a5d1aa5b156842a4d395c2278b2255
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-19 15:24:23 -0300

    :wrench: feat: handle search movie

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,29 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import * as S from './style';
 
 export default function Header() {
+    const [movie, setMovie] = useState(String);
+    const navigate = useNavigate();
+
+    function handleSearchMovie() {
+        navigate(`/search/${movie.trim()}`);
+    }
+
     return (
         <S.HeaderMain>
             <h1>
                 <Link to="/">CineTrending</Link>
             </h1>
 
-            <S.SearchMovie>
-                <input type="text" placeholder="search movie" />
-                <button type="button" aria-label="search movie">
+            <S.SearchMovie onSubmit={handleSearchMovie}>
+                <input
+                    type="text"
+                    placeholder="search movie"
+                    onChange={e => setMovie(e.target.value)}
+                />
+                <button type="submit" aria-label="search movie">
                     <i className="fas fa-search" />
                 </button>
             </S.SearchMovie>
